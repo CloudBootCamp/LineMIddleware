5396a26c84e714cd561f981f2cfb1d61a7bd2624
Rizwan Ahmed <rizwan.ahmed@live.com>
2018-03-30 00:21:41 +0500

    Add chocolate.jpeg.

--- index.js
+++ index.js
@@ -2,7 +2,13 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-
+var mysql      = require('mysql');
+var con = mysql.createConnection({
+  host     : 'db4free.net',
+  user     : 'linesa',
+  password : '654321',
+  database : 'linedb'
+});
 // create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -40,6 +46,67 @@ function handleEvent(event) {
 
   // use reply API
   return client.replyMessage(event.replyToken, echo);
+
+
+  
+}
+
+// event handler
+function handleEvent2(event) {
+  console.log('***handleEvent2***');
+ // make to connection to the database.
+con.connect(function(err) {
+ if (err) throw err;
+ // if connection is successful
+ con.query("SELECT * FROM SKU", function (err, result, fields) {
+   // if any error while executing above query, throw error
+   if (err) throw err;
+   // if there is no error, you have the result
+
+   console.log('***handleEvent After Result***');
+   console.log(result);
+
+   
+   // if there is no error, you have the fields object
+   // iterate for all the rows in fields object
+   Object.keys(fields).forEach(function(key) {
+     var field = fields[key];
+     console.log(field)
+   });
+   
+  /* for(var row in result){
+            console.log(row);
+           for(var column in result[row]){
+               str+=result[row][column] + ";";       
+           }
+           str+="-";
+         
+       }
+       */
+   //
+   
+   /*console.log('***handleEvent2***ForLooop');
+   var str='';
+   for(var row in result){
+           
+           for(var column in result[row]){
+               str+=result[row][column] + ";";       
+           }
+           str+="-";         
+       }
+   
+   console.log(str);
+   */
+  // create a echoing text message
+ //const echo = { type: 'text', text: result };
+
+ // use reply API
+ //return client.replyMessage(event.replyToken, echo);
+   
+   
+   //
+ });
+});
 }
 
 // listen on port
