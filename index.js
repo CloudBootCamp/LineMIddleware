f838bd17dc161fc960cbe09e0d7c8a17fd04d94f
CloudBootCamp <37856835+CloudBootCamp@users.noreply.github.com>
2018-03-31 07:09:55 +0500

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 // about the middleware, please refer to doc
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
-    .all(req.body.events.map(handleEvent2))
+    .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
